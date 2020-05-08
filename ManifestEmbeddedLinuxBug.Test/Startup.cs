4dbb78fbf43a12e5d4090294e315d4e91776635d
Daniel Khodabakhsh <dkhodabakhsh@pnimedia.com>
2020-05-07 18:17:27 -0700

    Change project from 2.2 to 2.1 (LTS).

--- ManifestEmbeddedLinuxBug.Test/Startup.cs
+++ ManifestEmbeddedLinuxBug.Test/Startup.cs
@@ -16,7 +16,7 @@ namespace ManifestEmbeddedLinuxBug
         {
             services
                 .AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
